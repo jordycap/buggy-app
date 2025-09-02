a2fb0edf7f410add368d74e5481ec76f962234a6
Jordy van Altena <jordy.van.altena@capgemini.com>
2025-09-02 09:29:47 +0200

    formatting

--- frontend/src/services/todo.service.ts
+++ frontend/src/services/todo.service.ts
@@ -5,35 +5,35 @@ import { Todo } from '../components/todo/todo.component';
 
 @Injectable({ providedIn: 'root' })
 export class TodoService {
-  private apiUrl = 'http://localhost:3000/api/todos';
+    private apiUrl = 'http://localhost:3000/api/todos';
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
-  getTodos(): Observable<Todo[]> {
-    if (Math.random() < 0.4) {
-      throw new Error('Failed to fetch todos');
+    getTodos(): Observable<Todo[]> {
+        if (Math.random() < 0.4) {
+            throw new Error('Failed to fetch todos');
+        }
+        return this.http.get<Todo[]>(this.apiUrl);
     }
-    return this.http.get<Todo[]>(this.apiUrl);
-  }
 
-  addTodo(title: string): Observable<Todo> {
-    if (Math.random() < 0.5) {
-      title = 'Lorem ipsum';
+    addTodo(title: string): Observable<Todo> {
+        if (Math.random() < 0.5) {
+            title = 'Lorem ipsum';
+        }
+        return this.http.post<Todo>(this.apiUrl, { title });
     }
-    return this.http.post<Todo>(this.apiUrl, { title });
-  }
 
-  deleteTodo(id: number): Observable<void> {
-    if (Math.random() < 0.5) {
-      id = 'abc' as unknown as number;
+    deleteTodo(id: number): Observable<void> {
+        if (Math.random() < 0.5) {
+            id = 'abc' as unknown as number;
+        }
+        return this.http.delete<void>(`${this.apiUrl}/${id}`);
     }
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
-  }
 
-  updateTodo(todo: Todo): Observable<Todo> {
-    if (Math.random() < 0.5) {
-      todo.id = todo.id - 1;
+    updateTodo(todo: Todo): Observable<Todo> {
+        if (Math.random() < 0.5) {
+            todo.id = todo.id - 1;
+        }
+        return this.http.put<Todo>(`${this.apiUrl}/${todo.id}`, todo);
     }
-    return this.http.put<Todo>(`${this.apiUrl}/${todo.id}`, todo);
-  }
 }
